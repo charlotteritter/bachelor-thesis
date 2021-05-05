cdf9abfbf2d2115f8aeadffd1f626320e2bc29ec
Charlotte <charlotte.ritter@fau.de>
2021-05-05 20:07:47 +0200

    Fixed the python file

--- LR_code_for_CharlotteRitter/CallGams.py
+++ LR_code_for_CharlotteRitter/CallGams.py
@@ -12,6 +12,6 @@ def call_process(command):
         print("Failed {0}".format(process))
 
 if __name__ == '__main__':
-    befehl = ['gams','TestLoop.gms','--SORTEDFILE==scenario_sorted_100_1_01']
+    befehl = ['gams','TestLoop.gms','--SORTEDFILE=scenario_sorted_100_1_01']
     call_process(befehl)
 
