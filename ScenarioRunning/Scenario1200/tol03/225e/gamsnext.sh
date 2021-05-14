c68977a3aa206c4e4b6a2debc9480f780fecc1af
Charlotte Ritter <mpwm031h@woody3.rrze.uni-erlangen.de>
2021-05-14 12:57:41 +0200

    07 files included

---
+++ ScenarioRunning/Scenario1200/tol03/225e/gamsnext.sh
@@ -0,0 +1,1 @@
+gmsgenus.run /home/hpc/mpwm/mpwm031h/bachelor-thesis/ScenarioRunning/Scenario1200/tol03/225e/ /home/hpc/mpwm/mpwm031h/bachelor-thesis/ScenarioRunning/Scenario1200/tol03/ $1 /home/hpc/mpwm/mpwm031h/bachelor-thesis/ScenarioRunning/Scenario1200/tol03/225e/gamscntr.dat /home/woody/mpwm/shared/software/gams24.8_linux_x64_64_sfx/ CPLEX
