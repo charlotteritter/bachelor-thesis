9df57c7913ed516e5422b25c29f6ecb31bf72d72
Charlotte Ritter <mpwm031h@woody3.rrze.uni-erlangen.de>
2021-05-17 08:46:06 +0200

    100_2 and 100_7 03 added

---
+++ SampleRun_Environment/600Scens/tol01/Scenario18/225a/gamsnext.sh
@@ -0,0 +1,1 @@
+gmsgenus.run /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/600Scens/tol01/Scenario18/225a/ /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/600Scens/tol01/Scenario18/ $1 /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/600Scens/tol01/Scenario18/225a/gamscntr.dat /home/woody/mpwm/shared/software/gams24.8_linux_x64_64_sfx/ CPLEX
---
+++ SampleRun_Environment/600Scens/tol07/Scenario13/225a/gamsnext.sh
@@ -0,0 +1,1 @@
+gmsgenus.run /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/600Scens/tol07/Scenario13/225a/ /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/600Scens/tol07/Scenario13/ $1 /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/600Scens/tol07/Scenario13/225a/gamscntr.dat /home/woody/mpwm/shared/software/gams24.8_linux_x64_64_sfx/ CPLEX
