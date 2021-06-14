5eaefd550d468271ff89817b59121f2c552b0981
Charlotte Ritter <mpwm031h@woody3.rrze.uni-erlangen.de>
2021-06-14 14:36:47 +0200

    Changed launcher

---
+++ SampleRun_Environment/100Scens/tol01/Scenario_15/225c/gamsnext.sh
@@ -0,0 +1,1 @@
+gmsgenus.run /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/100Scens/tol01/Scenario_15/225c/ /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/100Scens/tol01/Scenario_15/ $1 /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/100Scens/tol01/Scenario_15/225c/gamscntr.dat /home/woody/mpwm/shared/software/gams24.8_linux_x64_64_sfx/ CPLEX
---
+++ SampleRun_Environment/100Scens/tol01/Scenario_3/225c/gamsnext.sh
@@ -0,0 +1,1 @@
+gmsgenus.run /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/100Scens/tol01/Scenario_3/225c/ /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/100Scens/tol01/Scenario_3/ $1 /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/100Scens/tol01/Scenario_3/225c/gamscntr.dat /home/woody/mpwm/shared/software/gams24.8_linux_x64_64_sfx/ CPLEX
