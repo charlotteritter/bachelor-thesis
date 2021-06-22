315f237ab83096391122fa0e6761814595bb81d0
Charlotte Ritter <mpwm031h@woody3.rrze.uni-erlangen.de>
2021-06-22 09:20:21 +0200

    Initial commit with 600 01 03 05 and 07 runs with optca=optcr=0

---
+++ SampleRun_Environment/600Scens/tol05/Scenario9/225c/gamsnext.sh
@@ -0,0 +1,1 @@
+gmsgenus.run /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/600Scens/tol05/Scenario9/225c/ /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/600Scens/tol05/Scenario9/ $1 /home/hpc/mpwm/mpwm031h/bachelor-thesis/SampleRun_Environment/600Scens/tol05/Scenario9/225c/gamscntr.dat /home/woody/mpwm/shared/software/gams24.8_linux_x64_64_sfx/ CPLEX
